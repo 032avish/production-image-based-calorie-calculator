c0e40065bdb8c18c58d79df900ad3239ec0058d9
032avish <avishmittal2000@gmail.com>
2024-05-12 13:37:11 +0530

    commit changes
    Commit changes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import connectDB from './config/db.js';
 import foodRouter from './routes/foodRouter.js'
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from 'url';
 
 //configure env
 dotenv.config();
@@ -12,22 +11,17 @@ dotenv.config();
 //database config
 connectDB();
 
-//esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 //rest object
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'./build')))
+app.use(express.static(path.join(__dirname,'build')))
 
 //rest api
-app.use("*", function(req, res){
-    res.sendFile(path.join(__dirname,'./build/index.html'
+app.use("/", function(req, res){
+    res.sendFile(path.join(__dirname,'build','index.html'
     ));
   });
 
@@ -36,7 +30,7 @@ const PORT = process.env.PORT || 8080;
 
 
 //routes
-app.use("/api/v1/",foodRouter)
+app.use("/api/v1",foodRouter)
 
 
 //run listen
